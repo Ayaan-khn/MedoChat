c2d6d780d693e95aa5a1eb480dfb82b388b974fe
Ayaan-khn <mikezone94@gmail.com>
2026-06-28 00:20:55 +0530

    Setting up the env 4

--- Medo_chat/chat_app/app.py
+++ Medo_chat/chat_app/app.py
@@ -1,4 +1,22 @@
+import os
 import sqlite3
 
-conn = sqlite3.connect("database/medochat.db")
+# Folder containing app.py
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Go one level up (to Medo_chat)
+PROJECT_DIR = os.path.dirname(CURRENT_DIR)
+
+# Path to Medo_chat/database
+DATABASE_DIR = os.path.join(PROJECT_DIR, "database")
+os.makedirs(DATABASE_DIR, exist_ok=True)
+
+# Database file
+DB_PATH = os.path.join(DATABASE_DIR, "medochat.db")
+
+# Create the database
+conn = sqlite3.connect(DB_PATH)
 conn.close()
+
+print("Database created successfully!")
+print(DB_PATH)
