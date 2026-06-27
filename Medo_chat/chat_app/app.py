8a62bd2290217ddfc6eb52d27b9a5a110f972c33
Ayaan-khn <mikezone94@gmail.com>
2026-06-28 00:23:53 +0530

    Setting up the env 5

--- Medo_chat/chat_app/app.py
+++ Medo_chat/chat_app/app.py
@@ -20,3 +20,23 @@ conn.close()
 
 print("Database created successfully!")
 print(DB_PATH)
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
